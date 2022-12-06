d9284e0e0d2932e614350585496657352c68a1e3
Santy Sean A. Bactol <ssabactol18@gmail.com>
2022-12-06 09:53:13 +0800

    Add SPA and Map Dashboard to Admin Navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,48 +15,36 @@
                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     @if (Auth::user()->role == "admin")
                         <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="no-underline">
-                            {{-- <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg> --}}
                             {{ __('User Dashboard') }}
                         </x-nav-link>
                         <x-nav-link :href="route('enumerator.dashboard')" :active="request()->routeIs('enumerator.dashboard')" class="no-underline">
-                            {{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
-                                <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
-                            <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
-                            </svg> --}}
-                            {{ __('Farmer Dashboard') }}
-                        </x-nav-link>
-                    @elseif (Auth::user()->role == "enumerator")
-                        <x-nav-link :href="route('enumerator.welcome')" :active="request()->routeIs('enumerator.welcome')" class="no-underline">
-                            {{-- <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg> --}}
-                            {{ __('Home') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('enumerator.dashboard')" :active="request()->routeIs('enumerator.dashboard')" class="no-underline">
-                            {{-- <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
-                                <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
-                            <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
-                            </svg> --}}
                             {{ __('Farmer Dashboard') }}
                         </x-nav-link>
                         <x-nav-link :href="route('stresspronearea.dashboard')" :active="request()->routeIs('stresspronearea.dashboard')" class="no-underline">
-                            {{-- <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg> --}}
+                            {{ __('SPA Dashboard') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('enumerator.map')" :active="request()->routeIs('enumerator.map')" class="no-underline">
+                            {{ __('Map') }}
+                        </x-nav-link>
+                    @elseif (Auth::user()->role == "enumerator")
+                        <x-nav-link :href="route('enumerator.welcome')" :active="request()->routeIs('enumerator.welcome')" class="no-underline">
+                            {{ __('Home') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('enumerator.dashboard')" :active="request()->routeIs('enumerator.dashboard')" class="no-underline">
+                            {{ __('Farmer Dashboard') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('stresspronearea.dashboard')" :active="request()->routeIs('stresspronearea.dashboard')" class="no-underline">
                             {{ __('SPA Dashboard') }}
                         </x-nav-link>
                         <x-nav-link :href="route('enumerator.form')" :active="request()->routeIs('enumerator.form')" class="no-underline">
-                            {{-- <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg> --}}
                             {{ __('Farmer Forms') }}
                         </x-nav-link>
                         <x-nav-link :href="route('enumerator.form2')" :active="request()->routeIs('enumerator.form2')" class="no-underline">
-                            {{-- <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"></path></svg> --}}
                             {{ __('SPA Form') }}
                         </x-nav-link>
-
                         <x-nav-link :href="route('enumerator.map')" :active="request()->routeIs('enumerator.map')" class="no-underline">
-                            {{-- <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg> --}}
                             {{ __('Map') }}
                         </x-nav-link>
-                    
                     @endif
                </div>
            </div>
