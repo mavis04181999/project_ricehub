eae132cf8251e82a528a415764046fc6311ec28e
Santy Sean A. Bactol <ssabactol18@gmail.com>
2022-12-07 04:26:18 +0800

    Update StressProneArea Edit Feature 2

--- app/Http/Requests/StressProneAreaRequest.php
+++ app/Http/Requests/StressProneAreaRequest.php
@@ -24,8 +24,14 @@ class StressProneAreaRequest extends FormRequest
     public function rules()
     {
         return [
+            'input_spa_province' => ['required'],
+            'input_spa_city' => ['required_without:input_spa_municipality'],
+            'input_spa_municipality' => ['required_without:input_spa_city'],
+            'input_spa_barangay' => ['required'],
+
             'input_spa_stressecosystem' => ['required', 'array'],
             'input_spa_stressecosystem.*' => ['required'],
+
             'input_spa_totalfarmers' => ['required', 'numeric', 'min: 1'],
             'input_spa_totalstressarea' => ['required', 'numeric', 'min: 1'],
 
@@ -35,7 +41,16 @@ class StressProneAreaRequest extends FormRequest
     public function messages()
     {
         return [
+            'input_spa_province.required' => 'Province field is required.',
+            // 'input_spa_city.required' => 'City field is required.',
+            // 'input_spa_municipality.required' => 'Municipality is required.',
+            'input_spa_barangay.required' => 'Barangay field is required.',
+
+            'input_spa_city.required_without' => 'City Field is required when Municipality Field is not present',
+            'input_spa_municipality.required_without' => 'Municipality Field is required when City Field is not present',
+
             'input_spa_stressecosystem.required' => 'Stress Ecosystem field is required.',
+
             'input_spa_totalfarmers.min' => 'A.Farmer\'s Profile: Age field must be at least 1',
             'input_spa_totalstressarea.min' => 'A.Farmer\'s Profile: Age field must be at least 1',
         ];
--- app/Http/Requests/UpdateStressProneAreaRequest.php
+++ app/Http/Requests/UpdateStressProneAreaRequest.php
@@ -24,14 +24,8 @@ class UpdateStressProneAreaRequest extends FormRequest
     public function rules()
     {
         return [
-            'input_spa_province' => ['required'],
-            'input_spa_city' => ['required_without:input_spa_municipality'],
-            'input_spa_municipality' => ['required_without:input_spa_city'],
-            'input_spa_barangay' => ['required'],
-
             'input_spa_stressecosystem' => ['required', 'array'],
             'input_spa_stressecosystem.*' => ['required'],
-
             'input_spa_totalfarmers' => ['required', 'numeric', 'min: 1'],
             'input_spa_totalstressarea' => ['required', 'numeric', 'min: 1'],
 
@@ -41,14 +35,7 @@ class UpdateStressProneAreaRequest extends FormRequest
     public function messages()
     {
         return [
-            'input_spa_province.required' => 'Province field is required.',
-            'input_spa_barangay.required' => 'Barangay field is required.',
-
-            'input_spa_city.required_without' => 'City Field is required when Municipality Field is not present',
-            'input_spa_municipality.required_without' => 'Municipality Field is required when City Field is not present',
-
             'input_spa_stressecosystem.required' => 'Stress Ecosystem field is required.',
-
             'input_spa_totalfarmers.min' => 'A.Farmer\'s Profile: Age field must be at least 1',
             'input_spa_totalstressarea.min' => 'A.Farmer\'s Profile: Age field must be at least 1',
         ];
