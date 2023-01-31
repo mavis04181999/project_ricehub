281a12961366e86ef899efc700e63575a637d480
Santy Sean A. Bactol <ssabactol18@gmail.com>
2023-01-31 10:47:20 +0800

    Remove the mandatory status of Flooded or Submerge checkbox

--- resources/views/stresspronearea/form.blade.php
+++ resources/views/stresspronearea/form.blade.php
@@ -135,7 +135,7 @@
                                                 <div class="mt-4 space-y-4">
                                                     <div class="flex">
                                                         <div class="flex items-center mr-4">
-                                                            <input name="input_spa_stressecosystem[]" id="input_spa_stressecosystem_opt1" onclick="renderExtraPage(event)" type="checkbox" value="0" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" {{ empty(old('input_spa_stressecosystem')) ? '' : (in_array(0, old('input_spa_stressecosystem')) ? 'checked' : '')}} required>
+                                                            <input name="input_spa_stressecosystem[]" id="input_spa_stressecosystem_opt1" onclick="renderExtraPage(event)" type="checkbox" value="0" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" {{ empty(old('input_spa_stressecosystem')) ? '' : (in_array(0, old('input_spa_stressecosystem')) ? 'checked' : '')}}>
                                                             <label for="input_spa_stressecosystem_opt1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Flooded/Submerged</label>
                                                         </div>
                                                     </div>
